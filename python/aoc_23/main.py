5fd811351bb903f789a52c3d316b2c3343cddc67
GoldBrickLemon <9368670+goldbricklemon@users.noreply.github.com>
2022-12-27 19:54:29 +0100

    Templates

---
+++ python/aoc_23/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__':
+    with open("input.txt", "r") as f:
+        lines = f.readlines()
+    lines = [l.strip("\n").strip("\r") for l in lines]
+
+    print("#### Star one ####")
+
+    print("#### Star two ####")
